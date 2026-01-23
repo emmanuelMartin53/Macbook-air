4716959cd4a3ab1d06d3941791657a2fa2d804ce
Emmanuel Martin <em.martin456@gmail.com>
2026-01-23 12:29:28 +0100

    test on productInfos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
+import Configuration from "./components/Configuration";
 
 const App = () => {
   return (
     <div className="container">
       <div className="bg row d-flex pb-5">
       <Header />
+      <Configuration />
+
       </div>
     </div>
   );
---
+++ src/components/Configuration.jsx
@@ -0,0 +1,10 @@
+import ProductInfos from "./ProductInfos";
+
+const Configuration = () => {
+  return(
+    <div>
+      <ProductInfos />
+    </div>
+  )
+}
+export default Configuration;
