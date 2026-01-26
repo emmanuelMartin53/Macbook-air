426bdaa8f6d47697c2f336cfab3b017c4beee0ea
Emmanuel Martin <em.martin456@gmail.com>
2026-01-26 11:40:19 +0100

    install msw

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,9 @@
 // Active les matchers de jest-dom pour Testing Library
 import '@testing-library/jest-dom';
+
+import { beforeAll, afterEach, afterAll } from 'vitest'
+import { server } from './mocks/node.js'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
